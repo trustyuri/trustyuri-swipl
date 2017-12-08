ad09285d33dceb1bc27453895784f8b34c80af22
Tobias Kuhn <kuhntobias@gmail.com>
2017-12-08 10:45:20 +0100

    Rename file

--- scripts/CheckFile.sh
+++ scripts/CheckFile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
-swipl -f $DIR/trustyuri/CheckFile.pl -g run -t halt -- $*
+swipl -f $DIR/trustyuri/check_file.pl -g run -t halt -- $*
